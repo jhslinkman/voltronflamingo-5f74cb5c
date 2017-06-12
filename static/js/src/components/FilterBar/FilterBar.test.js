20cf16ed324e742a0d88f9cb2d680268b34d26a2
John Slinkman <jhslinkman@gmail.com>
2017-06-12 11:57:03 -0400

    Add unit test: FilterBar should be clickable

--- static/js/src/components/FilterBar/FilterBar.test.js
+++ static/js/src/components/FilterBar/FilterBar.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import sinon from 'sinon';
 import test from 'ava';
 import { shallow } from 'enzyme';
 import FilterBar from './FilterBar';
 
-function getWrapper() {
-  return shallow(<FilterBar/>);
+function getWrapper(onClickFilter) {
+  onClickFilter || (onClickFilter = sinon.spy());
+  return shallow(<FilterBar onClickFilter={onClickFilter}/>);
 }
 
 test('should get nav class', t => {
@@ -23,4 +25,18 @@ test('should include a filter Publishers', t => {
   const expected = 'Publishers';
   const filter = getWrapper().find('li').at(1);
   t.is(filter.text(), expected);
+});
+
+test('clicking Books filter should call onClickFilter with args ["books"]', t => {
+  const onClickFilter = sinon.spy();
+  const wrapper = getWrapper(onClickFilter);
+  wrapper.find('.books').find('a').simulate('click');
+  t.is(onClickFilter.calledWith('books'), true);
+});
+
+test('clicking Publishers filter should call onClickFilter with args ["publishers"]', t => {
+  const onClickFilter = sinon.spy();
+  const wrapper = getWrapper(onClickFilter);
+  wrapper.find('.publishers').find('a').simulate('click');
+  t.is(onClickFilter.calledWith('publishers'), true);
 });
