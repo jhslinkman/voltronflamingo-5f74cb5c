e5484a620b274fc853cfd6385be86130e33550b0
John Slinkman <jhslinkman@gmail.com>
2017-06-12 13:02:18 -0400

    Update PublisherItem component to display name

--- static/js/src/components/PublisherItem/PublisherItem.jsx
+++ static/js/src/components/PublisherItem/PublisherItem.jsx
@@ -7,12 +7,8 @@ const PublisherItem = props => {
 
   return (
     <div className={styles.item}>
-      <div className={styles.cover}>
-        <img className="t-publisher-cover" src={`https://placeimg.com/150/200/nature?id=${publisher.pk}`}/>
-      </div>
       <div className={`${styles.body}`}>
-        <span className="t-publisher-title">{publisher.title}</span>
-        <p className="t-publisher-description">{publisher.description}</p>
+        <span className="t-publisher-title">{publisher.name}</span>
       </div>
     </div>
   );
