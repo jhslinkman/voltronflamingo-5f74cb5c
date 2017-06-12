e3f185083981d2bd1b8a47d5b1b63f2ea18abe94
John Slinkman <jhslinkman@gmail.com>
2017-06-12 15:41:44 -0400

    Update PropTypes

--- static/js/src/components/PropTypes.js
+++ static/js/src/components/PropTypes.js
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types';
 
-const { arrayOf, number, shape, string } = PropTypes;
+const { arrayOf, number, shape, string, bool } = PropTypes;
 
 const book = shape({
   authors: arrayOf(number.isRequired),
   description: string.isRequired,
   pk: number.isRequired,
   publisher: number.isRequired,
-  title: string.isRequired
+  title: string.isRequired,
+  average_rating: number, // eslint-disable-line camelcase
+  rated: bool
 });
 
 const publisher = shape({
